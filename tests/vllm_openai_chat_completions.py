c5b010a32d5911d883622fcc104b49772ea5a018
kalinplus <142799819+kalinplus@users.noreply.github.com>
2025-06-25 14:41:31 +0800

    feat: init proj structure

---
+++ tests/vllm_openai_chat_completions.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+openai_api_key = "sk-xxx"
+openai_api_base = "http://localhost:8000/v1"
+
+client = OpenAI(
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+
+chat_outputs = client.chat.completions.create(
+    model="Qwen3-8B",
+    messages=[
+        {"role": "user", "content": "什么是深度学习"},
+    ]
+)
+
+print(chat_outputs)
