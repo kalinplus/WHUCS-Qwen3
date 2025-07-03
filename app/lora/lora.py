87784422591bfb077d18cc83001c321067933147
kalinplus <142799819+kalinplus@users.noreply.github.com>
2025-07-03 10:16:40 +0800

    feat: lora trainer

---
+++ app/lora/lora.py
@@ -0,0 +1,70 @@
+import swanlab
+from swanlab.integration.transformers import SwanLabCallback
+from peft import LoraConfig, TaskType
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments
+
+"""
+加载模型
+"""
+tokenizer = AutoTokenizer.from_pretrained('/root/autodl-tmp/Qwen/Qwen3-8B-AWQ')
+model = AutoModelForCausalLM.from_pretrained('/root/autodl-tmp/Qwen/Qwen3-8B-AWQ', device_map='auto')
+
+"""
+预处理输入数据
+"""
+def preprocess(sample):
+    MAX_LENGTH = 1024  # 最大序列长度
+    input_ids, attention_mask, labels = [], [], []  # 返回值，分别是输入文本编码、注意力掩码、输出文本编码
+    # 1. 构建指令
+    instruction = tokenizer(
+        f"<s><|im_start|>system\n现在你要作为一个全能的社团管理系统AI助手，扮演各种角色<im_end>\n"
+        f"<|im_start|>user\n{sample['instruction'] + sample['input']}<|im_end|>\n"
+        f"<|im_start|>assistant\n<think>\n\n</think>\n\n",
+        add_special_tokens=False
+    )
+    # 2. 编码并拼接为输入序列
+    response = tokenizer(f"{sample['output']}", add_special_tokens=False)
+    # 拼接 instruction 和 response 的 input_ids, 并在末尾添加 eos token 作为标记结束的 token
+    input_ids = instruction['input_ids'] + response['input_ids'] + [tokenizer.pad_token_id]
+    # 注意力掩码，表示模型需要关注的部分
+    attention_mask = instruction['attention_mask'] + response['attention_mask'] + [1]
+    # 3. 构造标签用于计算损失, 对于 instruction，使用 -100 表示这些位置无需计算 loss (无需预测)
+    labels = [-100] * len(instruction['input_ids']) + response['input_ids'] + [tokenizer.pad_token_id]
+
+    if len(input_ids) > MAX_LENGTH:  # 超出最大长度截断
+        input_ids = input_ids[:MAX_LENGTH]
+        attention_mask = attention_mask[:MAX_LENGTH]
+        labels = labels[:MAX_LENGTH]
+
+    return {
+        'input_ids': input_ids,
+        'attention_mask': attention_mask,
+        'labels': labels
+    }    
+
+
+config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM,  # 模型类型，CAUSAL_LM 表示因果语言模型
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],  # 需要训练的模型层的名字
+    r=8,  # LoRA 的秩，决定了低秩矩阵的维度
+    lora_alpha=32,  # 缩放参数，与 r 一起 决定了 LoRA 更新的强度。实际缩放比例为 lora_alpha / r = 4 倍
+    lora_dropout=0.1,  # 用于 LoRA 层的 Dropout 比例
+)
+
+args = TrainingArguments(
+    output_dir="/root/autodl-tmp/Qwen/Qwen3-8B-AWQ-LoRA",
+    per_device_train_batch_size=4,  # 每张卡上的 batch_size
+    gradient_accumulation_steps=4,  # 梯度累积
+    logging_steps=10,
+    num_train_epochs=3,  # epoch
+    save_steps=100,
+    learning_rate=1e-4,
+    save_on_each_node=True,
+    gradient_checkpointing=True,
+    report_to="none",
+)
+
+swanlab_callback = SwanLabCallback(
+    project="Qwen3-8B-AWQ-Lora",
+    experiment_name="Qwen3-8B-AWQ-LoRA-experiment",
+)
