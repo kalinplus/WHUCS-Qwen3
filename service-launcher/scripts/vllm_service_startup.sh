e2680cb07846a83c8854e96e598783c469a71ae4
kalinplus <142799819+kalinplus@users.noreply.github.com>
2025-07-04 10:22:13 +0800

    feat: vllm startup script enable lora

--- service-launcher/scripts/vllm_service_startup.sh
+++ service-launcher/scripts/vllm_service_startup.sh
@@ -10,6 +10,9 @@ python -m vllm.entrypoints.openai.api_server \
 --max-model-len 8192 \
 --trust-remote-code \
 --port 8010 \
---quantization awq
+--quantization awq \
+--enable-lora \
+--max-loras 2 \
+--max-lora-rank 64
 
 echo "vllm started"
